b29df59d08492b27eeca78db1d4ba57e920b5139
Emirhan Bülbül <emirrhanbulbul@gmail.com>
2022-06-09 19:26:56 +0300

    Splash screen eklendi

--- app/src/main/java/com/dzdtech/primhesaplama/view/LoginActivity.kt
+++ app/src/main/java/com/dzdtech/primhesaplama/view/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.dzdtech.primhesaplama
+package com.dzdtech.primhesaplama.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dzdtech.primhesaplama.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
