cd31e03352efa0fe9f8b7a8479cbd25c84c4bc5d
Emirhan Bülbül <emirrhanbulbul@gmail.com>
2022-06-09 23:45:23 +0300

    Login ekranı temelleri atıldı. Logoya animasyon eklendi.

--- app/src/main/java/com/dzdtech/primhesaplama/view/LoginActivity.kt
+++ app/src/main/java/com/dzdtech/primhesaplama/view/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.dzdtech.primhesaplama.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import com.dzdtech.primhesaplama.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val logo = findViewById<ImageView>(R.id.logo)
+        val animation = AnimationUtils.loadAnimation(this,R.anim.shake)
+
+        logo.startAnimation(animation)
     }
 }
