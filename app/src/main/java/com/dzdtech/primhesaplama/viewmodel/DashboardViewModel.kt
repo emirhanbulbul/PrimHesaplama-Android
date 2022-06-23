d226ec568207efed3cff75b25b037f8bc576c35c
Emirhan Bülbül <emirrhanbulbul@gmail.com>
2022-06-23 21:10:20 +0300

    Squashed commit of the following:
    commit 0be72ef9ceb0d022359c265932165da6a15439f7
    Author: Emirhan Bülbül <emirrhanbulbul@gmail.com>
    Date:   Thu Jun 23 21:07:42 2022 +0300
    
        Dashboard ekranı tasarlandı. Mockapi bağlandı ve veriler ekrana gönderiliyor. Veriler yüklenirken ekranda progress bar çıkması ve kaybolması ayarlandı.
    
    commit 157d5def83712c22969a4c97c56fba1a5de3c641
    Author: Emirhan Bülbül <emirrhanbulbul@gmail.com>
    Date:   Sun Jun 19 12:09:38 2022 +0300
    
        dashboard ekranı tasarlandı.

--- app/src/main/java/com/dzdtech/primhesaplama/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/dzdtech/primhesaplama/viewmodel/DashboardViewModel.kt
@@ -1,7 +1,31 @@
 package com.dzdtech.primhesaplama.viewmodel
 
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dzdtech.primhesaplama.model.Dashboard
+import com.dzdtech.primhesaplama.services.RetrofitInstance
+import kotlinx.coroutines.launch
 
 class DashboardViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val TAG = "Dashboard View Model"
+    private var _data = MutableLiveData<Dashboard>()
+    val data: LiveData<Dashboard>
+    get() = _data
+
+    val progress:Boolean = false
+
+    fun getData() {
+        viewModelScope.launch {
+            try {
+                _data.value = RetrofitInstance.retrofitDashboardInstance.getDashboardData()
+            } catch (e: Exception) {
+                Log.e(TAG, "${e.message}")
+            }
+        }
+    }
+
 }
